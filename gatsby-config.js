6b6cfc38f1822af0a59d4ab4e80ff780da7d91da
adesouza1 <futebol2014@hotmail.com>
2022-11-10 11:49:38 -0300

    add style components

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        extensions: [`,md`,`.mdx`],
+        extensions: [`.md`,`.mdx`],
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-images`,
@@ -48,7 +48,7 @@ module.exports = {
           `muli\:400,400i,700,700i`
         ],
         display: 'swap'
-      },
+      }
     },
   ],
 };
